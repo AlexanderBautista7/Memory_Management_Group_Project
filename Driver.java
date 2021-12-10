d366cd8f2b43c7fc74e09a146938776781bc14e8
Gerardo Vargas-Hernandez <78391890+GerardoVH@users.noreply.github.com>
2021-12-10 12:41:06 -0500

    Adding Driver

--- Driver.java
+++ Driver.java
@@ -1,5 +1,8 @@
+import net.processmanagmentgui.swing.HomePage;
+
 public class Driver {
     public void main(String args[]) {
-
+        ProcessGenerator PCB = new ProcessGenerator();
+        HomePage GUI = new HomePage();
     }
 }
--- ProcessGenerator.java
+++ ProcessGenerator.java
@@ -9,24 +9,25 @@ public class ProcessGenerator {
 
     // creates numP amount of processes and adds them to ready queue
     public void AddProcess(int numP) {
-        while(numP != 0) {
+        while (numP != 0) {
             newQueue.add(new Process());
             numP--;
         }
 
     }
-    
+
     public ReadyQueue getQueue() {
         return newQueue;
     }
-    //test simulation displaying process in queue
+
+    // test simulation displaying process in queue
     public static void main(String[] args) {
 
         ProcessGenerator pg1 = new ProcessGenerator();
 
         pg1.AddProcess(5);
 
-        for(int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("Process ID: " + pg1.getQueue().getProcess(i).getBlock().getProcessID());
             System.out.println("Process Size: " + pg1.getQueue().getProcess(i).getMemorySize());
             System.out.println("Process Time: " + pg1.getQueue().getProcess(i).getTotalRuntime());
