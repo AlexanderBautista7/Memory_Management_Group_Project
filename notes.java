e6da3d0a24d683bbf5350d844402a291f833b62c
Gerardo Vargas-Hernandez <78391890+GerardoVH@users.noreply.github.com>
2021-12-09 20:44:18 -0500

    NOTES FROM 12/9/21
    Some of the notes we went over tonight

---
+++ notes.java
@@ -0,0 +1,34 @@
+
+
+
+/**
+ * Notes: No Implementation
+ * Having as input a Process Object
+ * Process Object will have the followiung: 
+ *          System Clock 
+ *          ID
+ *          Frame Size
+ *          Frame start 
+ *          Frame End
+ * 
+ * Have Access to Ready Queue:
+ *          Current Process
+ *          List of Process
+ */
+
+
+
+ /**
+  *   function:
+  *     Take the last element of the Queue
+        Process.startTimer()
+        Sotre this result in boolean varibale: rusult 
+        if(result){
+            Remove from the Queue.remove(PCB); 
+            //GUI.update()
+        }else{
+            Queue.remove(PCB);
+            Queue.add(PCB);
+            //GUI.update()
+        }
+  */
